d8a92b128f8cee38f0a6d1c05b253a4c9a8aba31
Sergio Ruy <sergio_ruy@hotmail.com>
2020-08-10 17:18:11 +0400

    Obtendo  dados da API no Back End.

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,3 +1,5 @@
+import { Live } from './../../../shared/model/live.model';
+import { LiveService } from './../../../shared/service/live.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  livesPrevious: Live[];
+  livesNext: Live[];
+
+  constructor(
+    public liveService: LiveService
+  ) { }
 
   ngOnInit(): void {
+    this.getLives();
   }
 
+  getLives(){
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+
+
+    this.liveService.getLivesWithFlag('next').subscribe(data => {
+        this.livesNext = data.content;
+        console.log(this.livesNext);
+      });
+  }
 }
